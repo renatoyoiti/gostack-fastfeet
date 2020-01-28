711b0f884de84a05560dfb544d358c083c9a82cf
Renato Yoiti <renatoyoiti@gmail.com>
2020-01-28 16:51:19 -0300

    feat: add connection to models

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,9 @@
 import Sequelize from 'sequelize';
 
+import User from '../app/models/User';
 import databaseConfig from '../config/database';
+
+const models = [User];
 
 class Database {
   constructor() {
@@ -9,6 +12,8 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
   }
 }
 
